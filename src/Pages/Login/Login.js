80f2f43c6c207401d2a0f2f7f9e8dbca01d45f2d
Raju <ashfaqraju27@gmail.com>
2022-11-25 23:54:05 +0600

    login works

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import login from '../../assets/images/login/login.svg'
+import login from '../../assets/images/login/login.svg';
+import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
 
 const Login = () => {
 
+    const {userLogin} = useContext(AuthContext);
+
     const handleLogin = event =>{
         event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password =  form.password.value;
+
+        userLogin(email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        });
+
+
     }
 
     return (
@@ -21,13 +35,13 @@ const Login = () => {
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" required/>
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required/>
                             <label className="label">
                                 <Link to=''  className="label-text-alt link link-hover">Forgot password?</Link>
                             </label>
